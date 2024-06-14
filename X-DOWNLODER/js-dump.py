87c34ad725e81042d4e234dea2238651acaedb06
psico-mulciber <158625834+Mulc1b3R@users.noreply.github.com>
2024-06-14 16:59:07 +0100

    Add files via upload

---
+++ X-DOWNLODER/js-dump.py
@@ -0,0 +1,26 @@
+import os
+import requests
+
+def download_js_files_from_list(file_path, output_dir):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    with open(file_path, 'r') as file:
+        urls = file.read().splitlines()
+
+        for url in urls:
+            response = requests.get(url)
+
+            if response.status_code == 200 and url.endswith('.js'):
+                filename = os.path.join(output_dir, url.split('/')[-1])
+
+                with open(filename, 'wb') as f:
+                    f.write(response.content)
+
+                print(f"Downloaded: {filename}")
+            else:
+                print(f"Failed to download CSS file from URL: {url}")
+
+# Replace 'url-list.txt' with the actual file path containing the list of URLs
+# Replace 'static/css' (output_dir) with the desired output directory
+download_js_files_from_list('url-list.txt', 'static/js/')
